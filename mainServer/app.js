47360315bb709404f2cfbe0f43b68f41913b1f34
Scott Uno <scottunogundersen@gmail.com>
2023-10-25 13:54:45 +0200

    Runneble on port 3000

--- mainServer/app.js
+++ mainServer/app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
-const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
+// listen for requests
+const port = 3000;
+app.listen(port);
+console.log(`Listening for request on port ${port}`);
 
 // database connection
-const dbURI = '';
+const dbURI = 'mongodb+srv://Test:Passord1@minveimain.pinxpui.mongodb.net/?retryWrites=true&w=majority';
+
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
+  
