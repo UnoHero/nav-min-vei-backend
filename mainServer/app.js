b323a69075c6231e8d5114ee8f0f2397e4b0d4ad
Scott Uno <scottunogundersen@gmail.com>
2023-10-25 12:06:48 +0200

    First commit

---
+++ mainServer/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+
+// database connection
+const dbURI = '';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
