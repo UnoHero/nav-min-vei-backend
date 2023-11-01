b0e11f8d1b82bc1c77d74b9a956e018d9ca437bb
mleitejens <mleitejensen@gmail.com>
2023-11-01 14:11:27 +0100

    Added a bit of regex, and used postman to get data

--- Skatt-Server/app.js
+++ Skatt-Server/app.js
@@ -4,9 +4,11 @@ const app = express()
 const port = 3000
 const mongoose = require("mongoose")
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-    console.log("request answered")
+app.get("/hent/:name", async (req, res) => {
+    const data = await test(req.params.name)
+    console.log(req.params)
+    console.log(data)
+    res.send(data)
 })
 
 const dbURI = "mongodb+srv://Test:Passord1@minveimain.pinxpui.mongodb.net/?retryWrites=true&w=majority"
@@ -15,26 +17,27 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
   .catch((err) => console.log(err));
 
 
-const client = new MongoClient(dbURI)
-
-test()
-
-async function test(){
+async function test(name){
+  const client = new MongoClient(dbURI)
   try {
+    
     const database = client.db("Min-Vei-main")
     const skatteetatten = database.collection("Skatteetaten")
 
     // Query for everything with the name "test"
-    const query = { name : "test" }
-   
+    const query = { name : name }
+    const query2 = { name : {"$regex": `.*${name}.*`, "$options": "i" } }
+
     // Execute query
     const result = await skatteetatten.findOne(query)
+    const results = await skatteetatten.find(query2).toArray()
     //Finds all documents in collection
     const allDocuments = await skatteetatten.find().toArray()
     // Print the document returned by findOne()
     console.log(result);
     console.log(allDocuments)
 
+    return {result, allDocuments, results}
   } finally {
     await client.close()
   }
