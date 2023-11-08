ec75445d76e7e07e89f32b8363ea571242d1e382
Scott Uno <scottunogundersen@gmail.com>
2023-11-08 11:11:53 +0100

    Created the routes folder and started working on routes.

---
+++ mainServer/routes/authRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+//in the "{}" write the different things fron controllers...
+const {
+    getNmae,
+
+} = require("../controllers/")
+
+const routes = express.Router()
+
+//this get wil get the persons name... so if they are loged inn there name can be showed in the topp right?
+router.get("/", getName)
+
+//Make more for the MinVei site... Mail, name and everything needed...
+router.get("/")
+
+module.exports = router
