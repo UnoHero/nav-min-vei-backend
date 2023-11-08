966792afb4e1992cbd055cd8d513f120ae5b3c33
Scott Uno <scottunogundersen@gmail.com>
2023-11-08 11:23:55 +0100

    redy to merge

--- mainServer/app.js
+++ mainServer/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes.js');
+const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 
 const app = express();
