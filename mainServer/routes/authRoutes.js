95953137120de7841295fbc7eb2fea860cd4587f
mleitejens <mleitejensen@gmail.com>
2023-12-06 10:31:08 +0100

    Added routes

--- mainServer/routes/authRoutes.js
+++ mainServer/routes/authRoutes.js
@@ -1,20 +1,8 @@
-const express = require("express")
+const { Router } = require('express');
+const authController = require("../controllers/controller");
 
-//in the "{}" write the different things fron controllers...
-/*
-const {
-    getNmae,
+const router = Router();
 
-} = require("../controllers/")
-
-const routes = express.Router()
-
-//this get wil get the persons name... so if they are loged inn there name can be showed in the topp right?
-router.get("/", getName)
-
-//Make more for the MinVei site... Mail, name and everything needed...
-router.get("/")
+router.get("/hent/:firstName", authController.get_info)
 
 module.exports = router
-
-*/
